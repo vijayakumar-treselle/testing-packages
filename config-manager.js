a6fc8955eadb1b6c42ed65b40d82cc12a64b3556
vijay <vijayakumar.sekar@treselle.com>
2017-01-24 18:42:53 +0530

    Initial commit

---
+++ config-manager.js
@@ -0,0 +1,26 @@
+/*global module:true, require:true */
+
+'use strict';
+
+global.env_variable = {};
+
+/*
+ * configure and start logging
+ * @param {Object} config The configuration object for defining dir: log directory, level: loglevel
+ * @return the created logger instance
+ */
+function configurationManager() {
+    var envDetail = {
+        "db_user": "testing_user",
+        "db_password": "testing_pass",
+        "frontend_host_url": "http://frontend.com",
+        "vml_host_url": "http://vml.com",
+        "microservice_host_url": "http://microservice.com"
+    };
+
+    env_variable = envDetail;
+
+    return envDetail;
+}
+
+exports.configurationManager = configurationManager;
